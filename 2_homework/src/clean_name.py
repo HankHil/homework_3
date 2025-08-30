3ced3827a36f1d3975c86b8eb43c6c7dfd920d5d
HankHil <xtorinx@yandex.ru>
2025-08-30 14:57:18 +0300

    add 2 homework

--- 2_homework/src/clean_name.py
+++ 2_homework/src/clean_name.py
@@ -7,8 +7,13 @@
 
 
 def clean_name(fio: str) -> str:
-    alf = [chr(x) for x in range(ord('а'), ord('я'))] + [chr(y) for y in range(ord('А'), ord('Я'))] + ['я', 'Я', ' ']
-    return ''.join([x for x in list(fio) if x in alf])
+    alphabet = []
+    for s in "йцукенгшщзхъфывапролджэячсмитьбюё":
+        alphabet.append(s)
+        alphabet.append(s.upper())
+    alphabet.append(" ")
+    #alf = [chr(x) for x in range(ord('а'), ord('я'))] + [chr(y) for y in range(ord('А'), ord('Я'))] + ['я', 'Я', ' ']
+    return ''.join([x for x in list(fio) if x in alphabet])
     # res =''
     # for x in list(fio):
     #     if x in alf:
